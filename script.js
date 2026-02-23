61754b24e8d852e92f56c35d6c6a49271ae6553d
jaine-rodrigues <jainerodrigues1706@gmail.com>
2026-02-23 13:13:21 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('btn1').addEventListener('click', function(){
+    document.getElementById('resultado').innerText = 'Você clicou no Botão 1!';
+})
+document.getElementById('btn2').addEventListener('click', function(){
+    document.getElementById('resultado').innerText = 'Você clicou no Botão 2!';
+})
+document.getElementById('btn3').addEventListener('click', function(){
+    document.getElementById('resultado').innerText = 'Você clicou no Botão 3!';
+})
