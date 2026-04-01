0dff458a93363d9274bd210467377daeee3e1fa9
jaine-rodrigues <jainerodrigues1706@gmail.com>
2026-04-01 02:41:42 -0300

    feature/img

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('btn1').addEventListener('click', function(){
-    document.getElementById('resultado').innerText = 'Você clicou no Botão 1!';
-})
-document.getElementById('btn2').addEventListener('click', function(){
-    document.getElementById('resultado').innerText = 'Você clicou no Botão 2!';
-})
-document.getElementById('btn3').addEventListener('click', function(){
-    document.getElementById('resultado').innerText = 'Você clicou no Botão 3!';
-})
